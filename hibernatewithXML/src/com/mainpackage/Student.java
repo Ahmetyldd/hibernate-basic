44a2077c8ba847f45a2bad634e570d63014a16dd
Ahmet Yıldırım <ahmetyldd@gmail.com>
2018-11-23 15:32:53 +0300

    son version

---
+++ hibernatewithXML/src/com/mainpackage/Student.java
@@ -0,0 +1,47 @@
+package com.mainpackage;
+
+
+public class Student {
+   
+   private int id;
+
+   
+   private String first_name;
+   private String last_name;
+   private int age;  
+
+   public Student() {}
+   
+   public int getId() {
+      return id;
+   }
+   
+   public void setId( int id ) {
+      this.id = id;
+   }
+   
+ 
+   public int getAge() {
+      return age;
+   }
+   
+   public void setAge( int age ) {
+      this.age = age;
+   }
+
+public String getFirst_name() {
+	return first_name;
+}
+
+public void setFirst_name(String first_name) {
+	this.first_name = first_name;
+}
+
+public String getLast_name() {
+	return last_name;
+}
+
+public void setLast_name(String last_name) {
+	this.last_name = last_name;
+}
+}
